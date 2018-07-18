cf824336285a0f3359b027384f2775eb981d114a
Adrienne Schueller <atschueller@gmail.com>
2018-07-17 22:30:02 -0400

    hover.css

---
+++ client/src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+    <footer>
+        <input />
+        <a href="#" class ="links">Contact Us</a>
+    </footer>
+  );
+  
+  export default Footer;
