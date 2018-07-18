4f0afffceee729a16e34794ab574f4aaa9e6aa52
Adrienne Schueller <atschueller@gmail.com>
2018-07-17 21:49:19 -0400

    breaking it down

---
+++ client/src/components/nav/nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Nav = () => (
+    <nav className="navbar">
+      <a href="#" className="nav-logo">Beauty Merger</a>
+        <button className="btn btn-sm">Login</button>
+        <input />
+        <button className="btn btn-sm"> Sign up</button>
+        <a className="links" href="#">Links</a>
+    </nav>
+  );
+  
+  export default Nav;
+  
