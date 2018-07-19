6a080ab9c9b6655cd744db3475af5f17b1bfdb2f
Adrienne Schueller <atschueller@gmail.com>
2018-07-18 21:04:27 -0400

    fixed one thing

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-    <h1 className="hello">Hello Beautiful...</h1>
+    <h4 className="hello">Hello Beautiful...</h4>
 }
 
 export default Header;
